12b299a90b808ef4ee1de3637a56ea4f6bd517cc
Dipraj paul <diprajpaul85@gmail.com>
2024-04-12 12:14:52 +0530

    nnn

---
+++ frontend/src/pages/Amenities/EngineerConsultation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EngineerConsultation() {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
+}
+
+export default EngineerConsultation;
